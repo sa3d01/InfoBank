0dd3334727749a89e452abf3003db52e9c2ad751
sa3d01 <sa3dsalem01@gmail.com>
2022-12-21 22:32:33 +0200

    .

--- Modules/Need/Http/Controllers/NeedController.php
+++ Modules/Need/Http/Controllers/NeedController.php
@@ -3,14 +3,10 @@
 namespace Modules\Need\Http\Controllers;
 
 use App\Http\Controllers\BaseApiController;
-use Exception;
-use GuzzleHttp\Exception\ConnectException;
-use GuzzleHttp\Exception\RequestException;
 use Illuminate\Http\Request;
 use JetBrains\PhpStorm\Pure;
 use Modules\Need\Http\Requests\StoreNeedRequestApi;
 use Modules\Need\Http\Services\NeedService;
-use Softonic\GraphQL\ClientBuilder;
 
 class NeedController extends BaseApiController
 {
@@ -20,28 +16,44 @@ class NeedController extends BaseApiController
     {
         $this->needService = new NeedService();
     }
-    public function sliders(){
+
+    public function sliders()
+    {
         return $this->successResponse($this->needService->getSliders());
     }
-    public function contents(){
+
+    public function contents()
+    {
         return $this->successResponse($this->needService->getContents());
     }
-    public function trainingCategories(){
+
+    public function trainingCategories()
+    {
         return $this->successResponse($this->needService->getTrainingCategories());
     }
-    public function trainingsTitles(){
+
+    public function trainingsTitles()
+    {
         return $this->successResponse($this->needService->getTrainingTitles());
     }
-    public function needs(Request $request){
+
+    public function needs(Request $request)
+    {
         return $this->successResponse($this->needService->getNeeds($request));
     }
-    public function show($id){
+
+    public function show($id)
+    {
         return $this->successResponse($this->needService->show($id));
     }
-    public function store(StoreNeedRequestApi $request){
+
+    public function store(StoreNeedRequestApi $request)
+    {
         return $this->successResponse($this->needService->store($request));
     }
-    public function cancel($id){
+
+    public function cancel($id)
+    {
         return $this->successResponse($this->needService->cancel($id));
     }
 
